d560c47ec624341260edf84e819893e7b400d9ff
michael.joyce <michael.joyce@thriveglobal.com>
2022-12-13 11:53:18 +0000

    Update version and changes for dev mode with updates

--- src/utils/hooks/useExpoUpdates.ts
+++ src/utils/hooks/useExpoUpdates.ts
@@ -1,15 +1,17 @@
 import * as Updates from "expo-updates";
 
 export const useExpoUpdates = () => {
-  setInterval(() => {
-    Updates.checkForUpdateAsync().then((e) => {
-      if (e.isAvailable) {
-        Updates.fetchUpdateAsync().then((e) => {
-          if (e.isNew) {
-            Updates.reloadAsync().then(() => console.log("refreshed"));
-          }
-        });
-      }
-    });
-  }, 5000);
+  if(!__DEV__) {
+    setInterval(() => {
+      Updates.checkForUpdateAsync().then((e) => {
+        if (e.isAvailable) {
+          Updates.fetchUpdateAsync().then((e) => {
+            if (e.isNew) {
+              Updates.reloadAsync().then(() => console.log("refreshed"));
+            }
+          });
+        }
+      });
+    }, 5000);
+  }
 };
